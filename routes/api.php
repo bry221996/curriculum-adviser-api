48daa976cbda341de7bc424c5d8e4ded98926735
Bryan Mulingbayan <bryan.mulingbayan96@gmail.com>
2021-02-14 10:54:50 +0800

    update endpoint

--- routes/api.php
+++ routes/api.php
@@ -20,4 +20,8 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
 
 Route::group(['prefix' => 'v1'], function () {
     Route::post('/auth/login', 'AuthControler@login');
+
+    Route::group(['middleware' => 'auth:sanctum'], function () {
+        Route::resource('/departments', 'DepartmentController');
+    });
 });
