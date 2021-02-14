7799f96972c2a493fe2fa4b49b0b89cfde9dc439
Bryan Mulingbayan <bryan.mulingbayan96@gmail.com>
2021-02-14 10:00:00 +0800

    override create token

--- app/Models/User.php
+++ app/Models/User.php
@@ -7,10 +7,13 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Spatie\Permission\Traits\HasRoles;
+use Laravel\Sanctum\HasApiTokens;
+use Laravel\Sanctum\NewAccessToken;
+use Illuminate\Support\Str;
 
 class User extends Authenticatable
 {
-    use HasFactory, Notifiable, HasRoles;
+    use HasFactory, Notifiable, HasRoles, HasApiTokens;
 
     /**
      * The attributes that are mass assignable.
@@ -37,4 +40,24 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    /**
+     * Create a new personal access token for the user.
+     *
+     * @param  string  $name
+     * @param  array  $abilities
+     * @return \Laravel\Sanctum\NewAccessToken
+     */
+    public function createToken(string $name, array $abilities = ['*'])
+    {
+        $token = $this->tokens()->updateOrCreate(
+            ['name' => $name],
+            [
+                'token' => hash('sha256', $plainTextToken = Str::random(40)),
+                'abilities' => $abilities,
+            ]
+        );
+
+        return new NewAccessToken($token, $token->id . '|' . $plainTextToken);
+    }
 }
