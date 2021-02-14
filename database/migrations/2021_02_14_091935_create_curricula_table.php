23a28949681a296835f9adfba3eb14ddca7cb7a4
Bryan Mulingbayan <bryan.mulingbayan96@gmail.com>
2021-02-14 20:05:37 +0800

    add curriculum migration

---
+++ database/migrations/2021_02_14_091935_create_curricula_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCurriculaTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('curricula', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('program_id');
+            $table->year('academic_year_start');
+            $table->year('academic_year_end');
+            $table->text('description');
+            $table->boolean('is_draft')->default(1);
+            $table->boolean('status')->default(0);
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('curricula');
+    }
+}
