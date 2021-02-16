e91dead3c6e2540222b6dc21f92dc455da059c8c
Bryan Mulingbayan <bryan.mulingbayan96@gmail.com>
2021-02-16 09:16:46 +0800

    add new model

--- app/Models/Semester.php
+++ app/Models/Semester.php
@@ -2,11 +2,15 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 use Illuminate\Support\Str;
 
 class Semester extends Model
 {
+    use HasFactory;
+
     protected $guarded = [];
 
     protected $hidden = ['created_at', 'updated_at'];
@@ -24,5 +28,15 @@ class Semester extends Model
         self::creating(function ($semester) {
             $semester->uuid = Str::uuid()->toString();
         });
+    }
+
+    public function courses(): BelongsToMany
+    {
+        return $this->belongsToMany(Course::class, 'semester_courses', 'semester_uuid', 'course_uuid', 'uuid', 'uuid');
+    }
+
+    public function getRouteKeyName(): string
+    {
+        return 'uuid';
     }
 }
